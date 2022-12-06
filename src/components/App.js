f2467f1da452170ebb6506a43bf8e87c99955846
nizhantiw <nizhantiw@gmail.com>
2022-12-06 16:19:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const[isToggle,setToggle]=useState(true);
   return (
     <div id="main">
-      <div id="welcome-div">
+    {isToggle && <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
       </div>
-      <div id="about-div">
+    }
+    {!isToggle && <div id="about-div">
         <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
         </h1>
       </div>
-      <button id="toggle">Toggle</button>
+}
+      <button id="toggle" onClick={()=>{ setToggle(!isToggle)}}>Toggle</button>
     </div>
   );
 };
